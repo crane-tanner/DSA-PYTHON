6e5b607701dfc6a2265888d679454d1bfdc6b47c
Tanner Crane <tcrane94@gmail.com>
2025-05-14 00:15:39 -0700

    Algorithm for removing node from BST. need to add separate functions for removing node with two children, one, and none

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -5,6 +5,7 @@ The key value of each node in the tree is less than every key value in its right
 than every key value in its left subtree
 """
 
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -12,7 +13,7 @@ class Node:
         self.right = None
 
 
-class BST:
+class BinarySearchTree:
     def __init__(self):
         self.root = None
 
@@ -35,3 +36,44 @@ class BST:
                 current = current.right
         return self
 
+    def contains(self, data):
+        if not self.root:
+            return False
+        current = self.root
+        while data != current.data:
+            if data < current.data:
+                if not current.left:
+                    return False
+                current = current.left
+            elif data > current.data:
+                if not current.right:
+                    return False
+                current = current.right
+            else:
+                return True
+        return "Node found containing: " + str(data)
+
+    def remove(self, data, start=None, parent=None):
+        current = start or self.root
+        while current and current.data != data:
+            parent = current
+            if data < current.data:
+                current = parent.left
+            else:
+                current = parent.right
+        if not current:
+            raise Exception("node not found")
+        elif not current.left and not current.right:
+            return self._remove_node_no_children(current, parent)
+        elif current.right and current.left:
+            return self._remove_node_two_children(current)
+        return self._remove_node_one_child(current,parent)
+
+
+new_tree = BinarySearchTree()
+new_tree.insert(4)
+new_tree.insert(2)
+new_tree.insert(3)
+new_tree.insert(7)
+print(new_tree)
+print(new_tree.contains(6))
